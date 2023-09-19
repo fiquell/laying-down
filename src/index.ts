07b3570690a6d94ac5c6fcf5d3bf2c9495a9c551
Fiquell <fiquellh@gmail.com>
2023-09-20 01:30:21 +0800

    Load custom styles

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { file, serve, spawn } from "bun"
 
 const server = serve({
     fetch(req: Request) {
+        const baseURL = "public" + new URL(req.url).pathname
         const url = new URL(req.url)
 
         if (url.pathname === "/") {
@@ -22,6 +23,9 @@ const server = serve({
             )
         }
 
+        return new Response(file(baseURL))
+    },
+    error() {
         return new Response("404!", { status: 404 })
     },
     port: 11856
