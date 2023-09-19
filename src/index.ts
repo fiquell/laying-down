f66429145e4002b3245cabb26ceeb87c080af34c
Fiquell <fiquellh@gmail.com>
2023-09-19 08:47:36 +0800

    Added suspend method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!")
+import { file, serve, spawn } from "bun"
+
+const server = serve({
+    fetch(req: Request) {
+        const url = new URL(req.url)
+
+        if (url.pathname === "/") {
+            return new Response(file("public/index.html"))
+        }
+
+        if (url.pathname === "/sleep") {
+            spawn(["systemctl", "suspend"])
+            return new Response(`<button type="button" disabled>Sleep</button>`)
+        }
+
+        return new Response("404!", { status: 404 })
+    },
+    port: 11856
+})
+
+console.log("laying-down listening on port:", server.port)
